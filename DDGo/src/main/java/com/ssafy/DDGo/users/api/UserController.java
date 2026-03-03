3b04de196d3a3c0f80f899d926f327daaa546ea4
DESKTOP-6OO0P6F\SSAFY <k58701@naver.com>
2026-03-04 00:30:55 +0900

    [S14P21D204-29][BE] feat: UserController에 내 정보 조회 api 추가

--- DDGo/src/main/java/com/ssafy/DDGo/users/api/UserController.java
+++ DDGo/src/main/java/com/ssafy/DDGo/users/api/UserController.java
@@ -5,6 +5,7 @@ import com.ssafy.DDGo.users.application.UserService;
 import com.ssafy.DDGo.users.dto.request.UserLoginRequest;
 import com.ssafy.DDGo.users.dto.response.UserLoginResponse;
 import com.ssafy.DDGo.users.dto.request.UserRegisterRequest;
+import com.ssafy.DDGo.users.dto.response.UserInfoResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -13,6 +14,8 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.security.core.Authentication;
 
 @RestController
 @RequestMapping("/v1/users")
@@ -32,5 +35,11 @@ public class UserController {
     public ResponseEntity<ApiResponse<UserLoginResponse>> login(@RequestBody @Valid UserLoginRequest request) {
         UserLoginResponse tokenResponse = userService.login(request);
         return ResponseEntity.ok(ApiResponse.success("로그인 성공", tokenResponse));
+    }
+
+    @GetMapping("/me")
+    public ResponseEntity<ApiResponse<UserInfoResponse>> getMyInfo(Authentication authentication) {
+        UserInfoResponse userInfo = userService.getUserInfo(authentication.getName());
+        return ResponseEntity.ok(ApiResponse.success("내 정보 조회 성공", userInfo));
     }
 }
