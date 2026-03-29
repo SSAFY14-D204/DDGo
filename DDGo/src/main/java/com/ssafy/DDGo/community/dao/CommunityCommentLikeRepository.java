490050b616686d9e373d68fe7de63e860fb07488
이건 <sedrick743@gmail.com>
2026-03-30 00:05:57 +0900

    [BE]fix : 커뮤니티 게시글 삭제 로직 변경

--- DDGo/src/main/java/com/ssafy/DDGo/community/dao/CommunityCommentLikeRepository.java
+++ DDGo/src/main/java/com/ssafy/DDGo/community/dao/CommunityCommentLikeRepository.java
@@ -2,6 +2,7 @@ package com.ssafy.DDGo.community.dao;
 
 import com.ssafy.DDGo.community.domain.CommunityCommentLike;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
@@ -17,6 +18,10 @@ public interface CommunityCommentLikeRepository extends JpaRepository<CommunityC
     void deleteByCommentIdAndUserId(Long commentId, Long userId);
 
     void deleteAllByCommentIdIn(Collection<Long> commentIds);
+
+    @Modifying(clearAutomatically = true, flushAutomatically = true)
+    @Query("DELETE FROM CommunityCommentLike l WHERE l.comment.post.id = :postId")
+    void deleteAllByPostId(@Param("postId") Long postId);
 
     @Query("SELECT l.comment.id FROM CommunityCommentLike l WHERE l.user.id = :userId AND l.comment.id IN :commentIds")
     Set<Long> findLikedCommentIds(@Param("userId") Long userId, @Param("commentIds") Collection<Long> commentIds);
